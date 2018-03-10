6814845acd6c92bb0b2856fb5a21222cb7e4cf83
akshar <takleakshar@gmail.com>
2018-03-09 20:55:59 -0800

    make it better

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+app.set('views', path.join(__dirname, '../client'));
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('/', (req, res) => res.render('index'))
+var port = 3000
+
+
+//
+var passport = require('passport')
+, FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+  clientID: "340100819812558",
+  clientSecret: "534199fb0a8251d6de3c0bd16bdb7914",
+  callbackURL: "https://invitation-system.herokuapp.com/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  User.findOrCreate(..., function(err, user) {
+    if (err) { return done(err); }
+    done(null, user);
+  });
+}
+));
+
+
+app.listen(port, function() {
+ console.log('running at localhost: ' + port);
+});
