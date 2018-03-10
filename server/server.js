77dcf8007a1cf28af7e1450169e150235cee8628
akshar <takleakshar@gmail.com>
2018-03-09 23:09:11 -0800

    better

--- server/server.js
+++ server/server.js
@@ -10,17 +10,30 @@ var port = 3000
 
 
 //
-// var passport = require('passport')
-// , FacebookStrategy = require('passport-facebook').Strategy;
+var passport = require('passport')
+, FacebookStrategy = require('passport-facebook').Strategy;
 
-// passport.use(new FacebookStrategy({
-//   clientID: "340100819812558",
-//   clientSecret: "534199fb0a8251d6de3c0bd16bdb7914",
-//   callbackURL: "https://invitation-system.herokuapp.com/auth/facebook/callback"
-// }
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "https://invitation-system.herokuapp.com/auth/facebook/callback"
+  }
+//   function(accessToken, refreshToken, profile, cb) {
+//     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+));
+ 
+app.get('/auth/facebook',
+passport.authenticate('facebook'));
 
-// ));
-
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook', { failureRedirect: '/login' }),
+function(req, res) {
+  // Successful authentication, redirect home.
+  res.redirect('/');
+});
 
 app.listen(port, function() {
  console.log('running at localhost: ' + port);
