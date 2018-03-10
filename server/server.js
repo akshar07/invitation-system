be23487a9c8572bfbb4ce6afa93b475b9fb7286a
akshar <takleakshar@gmail.com>
2018-03-09 23:35:44 -0800

    better

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.get('/auth/facebook',
 passport.authenticate('facebook'));
 
 app.get('/auth/facebook/callback',
-passport.authenticate('facebook', { failureRedirect: '/login' }),
+passport.authenticate('facebook', { failureRedirect: '/auth/facebook' }),
 function(req, res) {
   // Successful authentication, redirect home.
   res.redirect('/');
