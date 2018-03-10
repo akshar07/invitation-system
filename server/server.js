bb2366da2c8f38b809a6859083d99eae5755d9c7
akshar <takleakshar@gmail.com>
2018-03-09 23:53:38 -0800

    better

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path');
 var passport = require('passport')
-import FacebookStrategy from 'passport-facebook';
+const FacebookStrategy = require('passport-facebook').Strategy;
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
 
