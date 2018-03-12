997013b6af090617a76748ee0d8542871afb19c1
akshar <takleakshar@gmail.com>
2018-03-11 21:36:43 -0700

    start wprking

--- server/server.js
+++ server/server.js
@@ -11,18 +11,8 @@ app.use(express.static(path.join(__dirname, '../client')));
 
 
 var port = 3000
-
-const transformFacebookProfile = (profile) => ({
-    name: profile.name,
-    avatar: profile.picture.data.url,
-  });
 //
-app.get('/auth/facebook/callback',
-passport.authenticate('facebook', { successRedirect: '/home',
-failureRedirect: '/auth/facebook' }))
 
-app.get('/auth/facebook',
-passport.authenticate('facebook',{ scope :'email'}));
 //db
 const { Client } = require('pg');
 
@@ -35,7 +25,13 @@ client.connect();
 const db_creation_string=`CREATE TABLE IF NOT EXISTS invitations(id SERIAL PRIMARY KEY, created_at timestamp with time zone NOT NULL DEFAULT current_timestamp, updated_at timestamp NOT NULL DEFAULT current_timestamp, senderId TEXT, receiverId TEXT);
                         CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name TEXT, link TEXT, email TEXT);`;
 
+app.use(passport.initialize());
+app.use(passport.session());
 
+ 
+passport.serializeUser((user, done) => done(null, user));
+// Deserialize user from the sessions
+passport.deserializeUser((user, done) => done(null, user));
 //shortid
 passport.use(new FacebookStrategy({
     clientID: '340100819812558',
@@ -60,13 +56,9 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
- 
-passport.serializeUser((user, done) => done(null, user));
-// Deserialize user from the sessions
-passport.deserializeUser((user, done) => done(null, user));
 
-app.use(passport.initialize());
-app.use(passport.session());
+
+
 app.enable('trust proxy');
 const express_enforces_ssl = require('express-enforces-ssl');
 app.use(express_enforces_ssl());
@@ -82,7 +74,12 @@ app.get('/', (req, res) =>{
     res.render('index')
 })
 
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook', { successRedirect: '/home',
+failureRedirect: '/auth/facebook' }))
 
+app.get('/auth/facebook',
+passport.authenticate('facebook',{ scope :'email'}));
 
 app.get('/home',(req,res)=>{
 
