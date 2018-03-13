abc8191212f5e4bb7b0613235dee80c0306a4fb7
akshar <takleakshar@gmail.com>
2018-03-12 20:44:33 -0700

    auth

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ passport.use(new FacebookStrategy({
     clientID: '340100819812558',
     clientSecret: '534199fb0a8251d6de3c0bd16bdb7914',
     callbackURL: "https://invitation-system.herokuapp.com/auth/facebook/callback",
-    profileFields: ['emails'],
+    profileFields: ['id', 'displayName', 'photos', 'email'],
     enableProof: true
   },
   function(accessToken, refreshToken, profile, done) {
