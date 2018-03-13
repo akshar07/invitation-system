fa7d6843b66faeab0585a22c6389900770f52546
akshar <takleakshar@gmail.com>
2018-03-13 08:34:03 -0700

    auth

--- server/server.js
+++ server/server.js
@@ -87,7 +87,7 @@ app.get('/home',isLoggedIn,(req,res)=>{
     })
 })
 function isLoggedIn(req, res, next) {
-  
+  console.log(req.isAuthenticated())
       // if user is authenticated in the session, carry on
       if (req.isAuthenticated())
           return next();
