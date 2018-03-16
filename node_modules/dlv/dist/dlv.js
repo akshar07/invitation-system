80f4ec851e7ca3715632352a0ffd443be01bdf9e
akshar <takleakshar@gmail.com>
2018-03-15 21:49:56 -0700

    invites

---
+++ node_modules/dlv/dist/dlv.js
@@ -0,0 +1,2 @@
+function t(t,o,e,i){for(i=0,o=o.split?o.split("."):o;t&&i<o.length;)t=t[o[i++]];return void 0===t?e:t}module.exports=t;
+//# sourceMappingURL=dlv.js.map
--- server/server.js
+++ server/server.js
@@ -1,37 +1,39 @@
-const express = require('express')
-const app = express()
-var session = require('express-session');
-const path = require('path');
-var passport = require('passport')
-var shortid = require('shortid');
-const FacebookStrategy = require('passport-facebook').Strategy;
-const bodyParser=require('body-parser')
+const express = require("express");
+const app = express();
+var session = require("express-session");
+const path = require("path");
+var passport = require("passport");
+var shortid = require("shortid");
+const FacebookStrategy = require("passport-facebook").Strategy;
+const bodyParser = require("body-parser");
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../client'));
-app.use(express.static(path.join(__dirname, '../client')));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../client"));
+app.use(express.static(path.join(__dirname, "../client")));
 
-var port = 3000
-const { Client } = require('pg');
+var port = 3000;
+const { Client } = require("pg");
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: true
 });
 
 client.connect();
-const db_creation_string=`CREATE TABLE IF NOT EXISTS invitations(id SERIAL PRIMARY KEY, created_at TIMESTAMP, updated_at TIMESTAMP, senderId TEXT, sendermsg TEXT, receiverId TEXT);
+const db_creation_string = `CREATE TABLE IF NOT EXISTS invitations(id SERIAL PRIMARY KEY, created_at TIMESTAMP, updated_at TIMESTAMP, senderId TEXT, sendermsg TEXT, receiverId TEXT);
                         CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name TEXT, link TEXT, email TEXT);`;
 
-app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: true,
-  proxy: true, // add this line
-  cookie: {
-    secure: true,
-    maxAge: 3600000,
-  }
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+    proxy: true, // add this line
+    cookie: {
+      secure: true,
+      maxAge: 3600000
+    }
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -39,99 +41,137 @@ passport.serializeUser((user, done) => done(null, user));
 // Deserialize user from the sessions
 passport.deserializeUser((user, done) => done(null, user));
 //shortid
-passport.use(new FacebookStrategy({
-    clientID: '340100819812558',
-    clientSecret: '534199fb0a8251d6de3c0bd16bdb7914',
-    callbackURL: "https://invitation-system.herokuapp.com/auth/facebook/callback",
-    profileFields: ['id', 'displayName', 'photos', 'email'],
-    enableProof: true
-  },
-  function(accessToken, refreshToken, profile, done) {
-    let pro_email=profile.emails[0].value;
-    client.query(`SELECT * FROM users WHERE email='${pro_email}'`,(err,res)=>{
-        if(err){console.log(err)}
-        if(res.rows.length >=1){console.log("ran"); done(null, res);}
-        else{ 
-          console.log("yep");
-            let shortId=shortid.generate();
-            client.query(`INSERT INTO users (name, link, email) VALUES ('${profile.displayName}','${shortId}','${pro_email}')`,(err,res)=>{
-              if(err){console.log(err)}
-              else{
-                done(null, res);
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: "340100819812558",
+      clientSecret: "534199fb0a8251d6de3c0bd16bdb7914",
+      callbackURL:
+        "https://invitation-system.herokuapp.com/auth/facebook/callback",
+      profileFields: ["id", "displayName", "photos", "email"],
+      enableProof: true
+    },
+    function(accessToken, refreshToken, profile, done) {
+      let pro_email = profile.emails[0].value;
+      client.query(
+        `SELECT * FROM users WHERE email='${pro_email}'`,
+        (err, res) => {
+          if (err) {
+            console.log(err);
+          }
+          if (res.rows.length >= 1) {
+            console.log("ran");
+            done(null, res);
+          } else {
+            console.log("yep");
+            let shortId = shortid.generate();
+            client.query(
+              `INSERT INTO users (name, link, email) VALUES ('${
+                profile.displayName
+              }','${shortId}','${pro_email}')`,
+              (err, res) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  done(null, res);
+                }
               }
-            })
+            );
+          }
         }
-    })
-  }
-));
+      );
+    }
+  )
+);
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.enable('trust proxy');
-const express_enforces_ssl = require('express-enforces-ssl');
+app.enable("trust proxy");
+const express_enforces_ssl = require("express-enforces-ssl");
 app.use(express_enforces_ssl());
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true
+  })
+);
 
-app.get('/', (req, res) =>{ 
-    client.query(db_creation_string, (err, res) => {
-        if (err) {
-          console.log(err)
-        } else {
-          console.log("done" + res)
-        }
-    });
-    res.render('index')
-})
-
-app.get('/auth/facebook/callback',
-passport.authenticate('facebook', { successRedirect: '/home',
-failureRedirect: '/auth/facebook' }))
-
-app.get('/auth/facebook',
-passport.authenticate('facebook',{scope:'email'}));
-
-app.get('/home',isLoggedIn,(req,res)=>{
-  console.log(req.user)
-    res.render('home',{
-      name:req.user.rows[0].name,
-      link:req.user.rows[0].link,
-      email:req.user.rows[0].email
-    })
-})
-app.post('/invite',(req,res)=>{
-  let senderId=req.body.name, sendermsg="Hi you have been invited", receiverId=req.body.to;
-  let current= new Date().toLocaleDateString();
-  client.query(`INSERT INTO invitations (created_at,updated_at,senderId,sendermsg,receiverId) VALUES ('${current}','${current}','${senderId}','${sendermsg}','${receiverId}')`,(err,result)=>{
-    if(err){
+app.get("/", (req, res) => {
+  client.query(db_creation_string, (err, res) => {
+    if (err) {
       console.log(err);
+    } else {
+      console.log("done" + res);
     }
-    else{
-      res.send("invited");
+  });
+  res.render("index");
+});
+
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: "/home",
+    failureRedirect: "/auth/facebook"
+  })
+);
+
+app.get(
+  "/auth/facebook",
+  passport.authenticate("facebook", { scope: "email" })
+);
+
+app.get("/home", isLoggedIn, (req, res) => {
+  console.log(req.user);
+  res.render("home", {
+    name: req.user.rows[0].name,
+    link: req.user.rows[0].link,
+    email: req.user.rows[0].email
+  });
+});
+app.post("/invite", (req, res) => {
+  let senderId = req.body.name,
+    sendermsg = req.body.msg,
+    receiverId = req.body.to;
+  let current = new Date().toLocaleDateString();
+  client.query(
+    `INSERT INTO invitations (created_at,updated_at,senderId,sendermsg,receiverId) VALUES ('${current}','${current}','${senderId}','${sendermsg}','${receiverId}')`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("invited");
+      }
     }
-  })
-  
-})
-app.get('/myInvitations',(req,res)=>{
-  client.query(`SELECT * from invitations where senderId='Akshar Takle'`,(err,doc)=>{
-    if(err){console.log(err)}
-    else{console.log(doc);res.render('invitations',{invites:doc.rows})}
-  })
-})
+  );
+});
+app.get("/myInvitations", (req, res) => {
+  client.query(
+    `SELECT * from invitations where senderId='Akshar Takle'`,
+    (err, doc) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(doc);
+        res.render("invitations", { invites: doc.rows });
+      }
+    }
+  );
+});
 function isLoggedIn(req, res, next) {
-  console.log(req.isAuthenticated())
-      // if user is authenticated in the session, carry on
-      if (req.isAuthenticated())
-          return next();
-  
-      // if they aren't redirect them to the home page
-      res.redirect('/');
-  }
+  console.log(req.isAuthenticated());
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect("/");
+}
 app.listen(process.env.PORT, function() {
- console.log('running at localhost: ' + port);
+  console.log("running at localhost: " + port);
 });
