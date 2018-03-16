3f2ebb72feef89ea83727fb9df1eec4a8c5e271d
akshar <takleakshar@gmail.com>
2018-03-16 01:18:31 -0700

    invites

--- server/server.js
+++ server/server.js
@@ -157,6 +157,7 @@ app.post("/invite", (req, res) => {
 });
 app.get("/myInvitations", (req, res) => {
   let link=req.body.link;
+  console.log(link)
   client.query(
     `SELECT * from invitations where senderId='${link}'`,
     (err, doc) => {
